c8e08e5684b35a569532723c1155b3d7a7170609
Skaivolker <rmt.codemail@gmail.com>
2021-02-20 13:48:26 +0200

    to postman smt

--- src/main/java/com/example/demo/rest/CRUDRest.java
+++ src/main/java/com/example/demo/rest/CRUDRest.java
@@ -1,14 +1,52 @@
 package com.example.demo.rest;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CRUDRest {
 
-    @GetMapping("/name")
-    public String getName(){
-        return "Name";
+    List<User> users = new ArrayList<>();
+
+    @GetMapping("/name/{personal-id}")
+    public String getName(@PathVariable("id") String id){
+        return "Name " + id;
     }
+
+    @PostMapping("/user/new")
+    public String createUser(@RequestBody User user){
+        users.add(user);
+        return "OK " + user.getName();
+    }
+
+    @PutMapping
+    public String updateUser(@RequestBody User user){
+        users.add(user);
+        for (User user1 : users) {
+            if(user1.getName().equals(user.getName())){
+                user1.setEmail(user.getEmail());
+            }
+        }
+        return "OK " + user.getName();
+    }
+
+    @GetMapping("/all")
+    public List<User> getAllUsers(){
+        return users;
+    }
+
+    @DeleteMapping("/name/{name}")
+    public String deleteUser(@PathVariable("name") String name){
+        for (User user : users){
+            if (user.getName().equals(name)){
+                users.remove(user);
+                break;
+            }
+        }
+        return "OK";
+    }
+
 }
---
+++ src/main/java/com/example/demo/rest/User.java
@@ -0,0 +1,11 @@
+package com.example.demo.rest;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    public String name;
+    public String email;
+}
