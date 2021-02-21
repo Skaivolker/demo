ce1777a8cf8e34043a2c0a55c176772a5016e753
Skaivolker <rmt.codemail@gmail.com>
2021-02-21 19:54:22 +0200

    smt

--- src/main/java/com/example/demo/th/HelloControl.java
+++ src/main/java/com/example/demo/th/HelloControl.java
@@ -5,13 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class HelloControl {
+
+    List<User> users = new ArrayList<>();
 
     @GetMapping("/hello")
     public String showHello(final ModelMap modelMap) {
         modelMap.addAttribute("text", "Welcome!");
         modelMap.addAttribute("user", new User());
+        modelMap.addAttribute("users", users);
         return "welcome";
     }
 }
